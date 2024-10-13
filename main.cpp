1d20e3dc2f7d4f9d368c7becbfdf7cb892366e03
Vincent Simard-Schmidt <vince.smdt@gmail.com>
2024-10-13 13:57:48 -0400

    Add imgui

--- main.cpp
+++ main.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
 #include <cstdint>
+#include <imgui.h>
+
+union Test {
+    struct {
+        uint8_t x;
+        uint8_t y;
+        uint8_t z;
+    };
+    uint8_t data[3];
+};
 
 int main() {
-    union Test {
-        struct {
-            uint8_t x;
-            uint8_t y;
-            uint8_t z;
-        };
-        uint8_t data[3];
-    };
+    IMGUI_CHECKVERSION();
 
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Running ImGui v" << ImGui::GetVersion() << std::endl;
 
     constexpr Test a = {65, 66, 67};
-
     for (const unsigned char i : a.data) {
         std::cout << i << std::endl;
     }
-
     std::cout << a.x << a.y << a.z << std::endl;
 
     return 0;
