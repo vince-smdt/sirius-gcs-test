0e1db8bcbc78707ee5d6320ba82f78cafa70cfbd
Vincent Simard-Schmidt <vince.smdt@gmail.com>
2024-10-14 00:56:57 -0400

    Add glfw/glad with conan

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,42 @@
 /// \file
 
 #include <iostream>
-#include <cstdint>
-#include <imgui.h>
-
-/// A test union
-/*!
-  Contains a struct and an uint8_t array, both sharing the same memory space.
- */
-union Test {
-    struct {
-        uint8_t x;
-        uint8_t y;
-        uint8_t z;
-    };
-    uint8_t data[3]; ///< This contains xyz data
-};
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 /// The program's entry point.
 int main() {
-    IMGUI_CHECKVERSION();
+    glfwInit();
 
-    std::cout << "Running ImGui v" << ImGui::GetVersion() << std::endl;
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    constexpr Test a = {65, 66, 67};
-    for (const unsigned char i : a.data) {
-        std::cout << i << std::endl;
+    GLFWwindow* window = glfwCreateWindow(800, 800, "sirius-gcs", nullptr, nullptr);
+    if (window == nullptr) {
+        std::cout << "noob";
+        glfwTerminate();
+        return 1;
     }
-    std::cout << a.x << a.y << a.z << std::endl;
+    glfwMakeContextCurrent(window);
 
-    return 0;
+    gladLoadGL();
+
+    glViewport(0, 0, 800, 800);
+
+    float bruh = 0.07;
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        bruh += 0.01;
+        if (bruh > 0.99) {
+            bruh = 0;
+        }
+        glClearColor(bruh, 0.13f, 0.17f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 }
