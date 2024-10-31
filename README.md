4b6c30fef38ae429dfb63c26f9562038c39340a7
vince-smdt <vince.smdt@gmail.com>
2024-10-30 22:25:14 -0400

    Add script to setup project

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # sirius-gcs
 
-Doc de setup à venir
+## Setup
+
+1. Run `git clone https://github.com/SiriusUdS/sirius-gcs.git`.
+2. Execute the `setup.bat` script in the **setup** folder.
+3. Open the **sirius-gcs.sln** solution in Visual Studio 2022.
+4. Compile and run.
---
+++ setup/setup.bat
@@ -0,0 +1,18 @@
+@echo off
+
+REM Install git submodules
+pushd %~dp0..
+echo Installing git submodules...
+git submodule update --init --recursive
+
+REM Install dependencies
+.\vcpkg\bootstrap-vcpkg.sh
+.\vcpkg\vcpkg install --host-triplet=x64-windows --x-install-root=vcpkg\installed
+.\vcpkg\vcpkg integrate install
+
+REM Generate project files
+.\tools\premake5 vs2022
+
+REM Go back to current directory
+popd
+popd
