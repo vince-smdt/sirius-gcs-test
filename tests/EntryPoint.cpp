f75cd58e8f8c9e7b3026ba97fb8ff3451808461e
vince-smdt <vince.smdt@gmail.com>
2024-11-23 21:14:21 -0500

    Add example test class

---
+++ tests/EntryPoint.cpp
@@ -0,0 +1,8 @@
+#include "TestExample.h"
+
+#include <iostream>
+
+int main(int, char*[]) {
+    TestExample test;
+    test.exampleTest();
+}
---
+++ tests/TestExample.h
@@ -0,0 +1,9 @@
+#ifndef TESTEXAMPLE_H
+#define TESTEXAMPLE_H
+
+class TestExample {
+public:
+    void exampleTest();
+};
+
+#endif // TESTEXAMPLE_H
